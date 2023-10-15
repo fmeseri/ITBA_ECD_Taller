c908f35b9f4d414cf1da174e3bda7d65d7512ec1
Fernando Meseri <fmeseri@gmail.com>
2023-10-14 21:35:37 -0300

    CleanUps

--- airflow/dags/Dolar_Historico_dag.py
+++ airflow/dags/Dolar_Historico_dag.py
@@ -10,13 +10,6 @@ from airflow.models import DAG
 from airflow.operators.email_operator import EmailOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.postgres_operator import PostgresOperator
-from airflow.operators.sqlite_operator import SqliteOperator
-from sqlite_cli import SqLiteClient
-from sqlalchemy import create_engine
-import psycopg2 
-from psycopg2.extras import execute_values
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
-
 from symbol import parameters 
 
 
